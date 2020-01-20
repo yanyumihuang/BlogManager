356457d0b6cd671c1bd7b05b0fcedd47dac45df2
sikunliang <sikunliang1234@gmail.com>
2020-01-20 11:37:47 +0800

    Initial commit

---
+++ src/main/java/info/yymh/blogmanager/BlogmanagerApplication.java
@@ -0,0 +1,13 @@
+package info.yymh.blogmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogmanagerApplication.class, args);
+    }
+
+}
